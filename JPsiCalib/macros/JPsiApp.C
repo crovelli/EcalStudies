119ba49cd959fc05b1917815a49e5fd8e82306a3
crovelli <>
2009-04-16 17:37:24 +0000

    final numbers + inv mass added

--- JPsiCalib/macros/JPsiApp.C
+++ JPsiCalib/macros/JPsiApp.C
@@ -18,12 +18,23 @@ int main(int argc, char* argv[]) {
   int applic = 1;
 
   char inputFileName[150];
-  if ( argc < 2 ){
-    std::cout << "missing argument: insert inputFile with list of root files" << std::endl; 
+  if ( argc < 3 ){
+    std::cout << "missing argument: " << std::endl;
+    std::cout << "insert A) inputFile with list of root files" << std::endl; 
+    std::cout << "insert B) sample = " << std::endl;
+    std::cout << "1) signal j/psi prompt"      << std::endl;
+    std::cout << "2) signal j/psi non prompt"  << std::endl;
+    std::cout << "3) QCD bc->e, 20-30"         << std::endl;
+    std::cout << "4) QCD bc->e, 30-80"         << std::endl;
+    std::cout << "5) QCD bc->e, 80-170"        << std::endl;
+    std::cout << "6) QCD em. enriched, 20-30"  << std::endl;
+    std::cout << "7) QCD em. enriched, 30-80"  << std::endl;
+    std::cout << "8) QCD em. enriched, 80-170" << std::endl;
     return 1;
   }
   strcpy(inputFileName,argv[1]);
-  
+  int theSample = atoi(argv[2]);
+
   TChain *theChain = new TChain("T1");
   char Buffer[500];
   char MyRootFile[2000];
@@ -50,7 +61,7 @@ int main(int argc, char* argv[]) {
 
   if (applic==1) {
     finalJPsiAnalysisEle jpsiStudy(theChain);
-    jpsiStudy.Loop();
+    jpsiStudy.Loop(theSample);
   }  
 
   if (applic==2) {
